ab05500cc0aec58024ee1e9ec9dba180bc6d76ec
Katrina Rosales <katrinarosales@icloud.com>
2024-11-11 22:38:32 +0100

    updating configuration

--- web-portfolio/next.config.ts
+++ web-portfolio/next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export',
+  basePath: isProd ? '/main' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
